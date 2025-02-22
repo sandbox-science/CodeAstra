ba7ce2d305bf811c657084e93d431438538013c1
Chris <rolletchris1@gmail.com>
2025-02-22 08:23:57 -0800

    [Update] Set Dev name and app name in main

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@ int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
     app.setApplicationVersion("0.0.1");
+    app.setOrganizationName("Chris Dedman");
+    app.setApplicationName("Code Astra");
 
     MainWindow window;
     window.show();
