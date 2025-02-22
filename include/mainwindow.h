8ec32e2ab7251291f966c3c3501cc5927982f5a0
Chris <rolletchris1@gmail.com>
2025-02-21 23:58:27 -0800

    init main window header

---
+++ include/mainwindow.h
@@ -0,0 +1,25 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include "codeeditor.h"
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    virtual ~MainWindow();
+
+private slots:
+    void openFile();
+    void saveFile();
+    void saveFileAs();
+
+private:
+    CodeEditor *editor;
+    QString currentFileName;
+    void createMenuBar();
+};
+#endif // MAINWINDOW_H
