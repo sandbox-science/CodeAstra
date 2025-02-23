27655080645982cd53fce615cdcc1ff767b6a5d3
Chris <rolletchris1@gmail.com>
2025-02-22 19:35:20 -0800

    [Feature] Added line count + current line highlight

---
+++ include/linenumber.h
@@ -0,0 +1,30 @@
+#ifndef LINENUMBER_H
+#define LINENUMBER_H
+
+#include "codeeditor.h"
+
+#include <QWidget>
+#include <QPaintEvent>
+#include <QSize>
+
+class LineNumberArea : public QWidget
+{
+public:
+  LineNumberArea(CodeEditor *editor) : QWidget(editor), codeEditor(editor) {}
+
+  QSize sizeHint() const override
+  {
+    return QSize(codeEditor->lineNumberAreaWidth(), 0);
+  }
+
+protected:
+  void paintEvent(QPaintEvent *event) override
+  {
+    codeEditor->lineNumberAreaPaintEvent(event);
+  }
+
+private:
+  CodeEditor *codeEditor;
+};
+
+#endif // LINENUMBER_H
