bd0758302f66fed83472de250eba84116f190856
Chris <rolletchris1@gmail.com>
2025-03-08 22:09:27 -0800

    [Feature] Syntax highlighting header file

---
+++ include/Syntax.h
@@ -0,0 +1,31 @@
+#ifndef SYNTAX_H
+#define SYNTAX_H
+
+#include <QTextCharFormat>
+#include <QRegularExpression>
+#include <QSyntaxHighlighter>
+#include <QTextDocument>
+
+class Syntax : public QSyntaxHighlighter
+{
+    Q_OBJECT
+
+public:
+    Syntax(QTextDocument *parent = nullptr);
+
+protected:
+    void highlightBlock(const QString &text) override;
+
+private:
+    struct SyntaxRule
+    {
+        QRegularExpression pattern;
+        QTextCharFormat format;
+    };
+    QList<SyntaxRule> syntaxRules;
+
+    QTextCharFormat keywordFormat;
+    QTextCharFormat singleLineCommentFormat;
+};
+
+#endif // SYNTAX_H
