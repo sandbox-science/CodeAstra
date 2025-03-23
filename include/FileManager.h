cc1c56fd99a00366e41c4e075f76339a6c0c5631
Chris <rolletchris1@gmail.com>
2025-03-22 18:09:17 -0700

    [Refactor] Adjust formatting and organization in FileManager class for improved readability + conged FileManager to instance

--- include/FileManager.h
+++ include/FileManager.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <QObject>
+#include <memory>
 
 class CodeEditor;
 class MainWindow;
@@ -11,21 +12,36 @@ class MainWindow;
  */
 class FileManager : public QObject
 {
-  Q_OBJECT
+    Q_OBJECT
 
 public:
-  FileManager(CodeEditor *editor, MainWindow *mainWindow);
-  ~FileManager();
+    static FileManager &getInstance(CodeEditor *editor = nullptr, MainWindow *mainWindow = nullptr)
+    {
+        static FileManager instance(editor, mainWindow);
+        return instance;
+    }
+
+    FileManager(const FileManager &) = delete;
+    FileManager &operator=(const FileManager &) = delete;
+
+    QString getFileExtension() const;
+    QString getCurrentFileName() const;
+
+    void setCurrentFileName(const QString fileName);
+    void initialize(CodeEditor *editor, MainWindow *mainWindow);
 
 public slots:
-  void newFile();
-  void saveFile();
-  void saveFileAs();
-  void openFile();
-  void loadFileInEditor(const QString &filePath);
+    void newFile();
+    void saveFile();
+    void saveFileAs();
+    void openFile();
+    void loadFileInEditor(const QString &filePath);
 
 private:
-  std::unique_ptr<CodeEditor> m_editor;
-  QString m_currentFileName;
-  MainWindow *m_mainWindow;
+    FileManager(CodeEditor *editor, MainWindow *mainWindow);
+    ~FileManager();
+
+    CodeEditor *m_editor;
+    MainWindow *m_mainWindow;
+    QString m_currentFileName;
 };
