e6199266aede87b8b11b0ca6b47ca1fdc1bbc032
Chris <rolletchris1@gmail.com>
2025-06-26 18:04:13 -0700

    [Add] added function header for file update detection

--- include/FileManager.h
+++ include/FileManager.h
@@ -58,6 +58,7 @@ public slots:
     void saveFileAs();
     void openFile();
     void loadFileInEditor(const QString &filePath);
+    bool isSaved(QString currentFileName);
 
     QString getDirectoryPath() const;
 
